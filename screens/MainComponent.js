76edc55c1ef3cca0eda730305426b3098c1ff77a
Mariel Goettsch <mlgoettsch@gmail.com>
2022-11-21 15:55:04 -0600

    Week 4 Secure Store

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -17,6 +17,7 @@ import { fetchPromotions } from '../features/promotions/promotionsSlice';
 import { fetchComments } from '../features/comments/commentsSlice';
 import ReservationScreen from './ReservationScreen';
 import FavoritesScreen from './FavoritesScreen';
+import LoginScreen from './LoginScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -139,6 +140,28 @@ const FavoritesNavigator = () => {
   );
 };
 
+const LoginNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name='Login'
+        component={LoginScreen}
+        options={({ navigation }) => ({
+            headerLeft: () => (
+                <Icon
+                    name='sign-in'
+                    type='font-awesome'
+                    iconStyle={styles.stackIcon}
+                    onPress={() => navigation.toggleDrawer()}
+                />
+            )
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
+
 const DirectoryNavigator = () => {
   const Stack = createStackNavigator();
   return (
@@ -210,6 +233,20 @@ const Main = () => {
           drawerContent={CustomDrawerContent}
         >  
           <Drawer.Screen
+            name='Login'
+            component={LoginNavigator}
+            options={{ 
+              drawerIcon: ({ color }) => (
+                <Icon
+                  name='sign-in'
+                  type='font-awesome'
+                  size={24}
+                  color= {color}
+                />  
+              )
+            }}
+          />
+          <Drawer.Screen
             name='Home'
             component={HomeNavigator}
             options={{ 
@@ -257,7 +294,7 @@ const Main = () => {
           />
            <Drawer.Screen
             name='Favorites'
-            component={ReservationNavigator}
+            component={FavoritesNavigator}
             options={{
               title: 'My Favorites',
               drawerIcon: ({ color }) => (
