36bc72c592976041b8a78e0953074b6d83749099
Mariel Goettsch <mlgoettsch@gmail.com>
2022-11-22 11:52:58 -0600

    Week 4 Local Notifications

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -3,6 +3,7 @@ import { Text, View, ScrollView, StyleSheet, Switch, Button, Alert } from 'react
 import { Picker } from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from "react-native-animatable";
+import * as Notifications from 'expo-notifications';
 
 const ReservationScreen = () => {
   const [campers, setCampers] = useState(1);
@@ -24,7 +25,7 @@ const ReservationScreen = () => {
       'Begin Search?',
       'Number of Campers: ' + campers + '\n' + 
       'Hike-In? ' + hikeIn + '\n' + 
-      'Date: ' + date,
+      'Date: ' + date.toLocaleDateString('en-US'),
       [
         {
             text: 'Cancel',
@@ -33,7 +34,10 @@ const ReservationScreen = () => {
         },
         {
             text: 'OK',
-            onPress: () => resetForm(),
+            onPress: () => {
+              presentLocalNotification(date.toLocaleDateString('en-US'));
+              resetForm();
+            }
         }
       ],
       { cancelable: false }
@@ -45,6 +49,36 @@ const ReservationScreen = () => {
     setHikeIn(false);
     setDate(new Date());
     setShowCalendar(false);
+  };
+
+  const presentLocalNotification = async (reservationDate) => {
+    const sendNotification = () => {
+      Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldShowAlert: true,
+          shouldPlaySound: true,
+          shouldSetBadge: true
+        })
+      });
+
+      Notifications.scheduleNotificationAsync({
+        content: {
+          title: 'Your Campsite Reservation Search',
+          body: `Search for ${reservationDate} requested`
+        },
+        trigger: null 
+        //causes notification to fire/trigger immediately
+      });
+    };
+    
+    //follow await (similar to then) with a promise. Can ONLY use await within an async fxn!
+    let permissions = await Notifications.getPermissionsAsync();
+    if (!permissions.granted) {
+      permissions = await Notifications.requestPermissionsAsync();
+    }
+    if (permissions.granted) {
+      sendNotification();
+    }
   };
 
   return (
