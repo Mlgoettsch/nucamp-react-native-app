fb1602dc3470d80358b8267077ce2bea4694db72
Mariel Goettsch <mlgoettsch@gmail.com>
2022-11-26 11:43:09 -0600

    Week 4 Assignments

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,6 +6,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import * as ImagePicker from 'expo-image-picker';
 import { baseUrl } from '../shared/baseUrl';
 import logo from '../assets/images/logo.png';
+import * as ImageManipulator from 'expo-image-manipulator';
 
 const LoginTab = ({ navigation }) => {
   const [username, setUsername] = useState("");
@@ -136,9 +137,8 @@ const RegisterTab = () => {
   };
 
   const getImageFromCamera = async () => {
-    const cameraPermission = 
-      await ImagePicker.requestCameraPermissionsAsync();
-
+    const cameraPermission = await ImagePicker.requestCameraPermissionsAsync();
+    console.log(cameraPermission);
     if(cameraPermission.status === 'granted') {
       const capturedImage = await ImagePicker.launchCameraAsync({
         allowsEditing: true,
@@ -146,10 +146,33 @@ const RegisterTab = () => {
       });
       if (!capturedImage.cancelled) {
         console.log(capturedImage);
-        setImageUrl(capturedImage.uri)
+        processImage(capturedImage.uri);
       }
     }
   }
+
+  const processImage = async (imgUri) => {
+    const processedImage = await ImageManipulator.manipulateAsync(
+      imgUri, [{ resize: {width: 400}}], { format: ImageManipulator.SaveFormat.PNG }
+      )
+      console.log(processedImage);
+      setImageUrl(processedImage.uri);
+  }
+
+  const getImageFromGallery = async () => {
+    const mediaLibraryPermissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    console.log(mediaLibraryPermissions.status);
+    if (mediaLibraryPermissions.status === 'granted') {
+      const capturedImage = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [1, 1],
+      });
+      if (!mediaLibraryPermissions.status) {
+        console.log(capturedImage);
+        processImage(capturedImage.uri);
+      }
+    }
+  };
 
   return (
     <ScrollView>
@@ -161,6 +184,7 @@ const RegisterTab = () => {
             style={styles.image}
           />
           <Button title='Camera' onPress={getImageFromCamera} />
+          <Button title='Gallery' onPress={getImageFromGallery}/>
         </View>
         <Input
           placeholder="Username"
